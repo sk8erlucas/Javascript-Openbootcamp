5d2bfe4473bd44679891b504321e46cde7367636
sk8erlucas <galileo.laino@gmail.com>
2022-10-18 09:50:09 -0300

    new file:   Leccion 7/fechas p.js 	modified:   Leccion 7/fechas.js

--- Leccion 7/fechas.js
+++ Leccion 7/fechas.js
@@ -1,34 +1,17 @@
 
-// Fechas
+let fecha_hoy = new Date()
+console.log(fecha_hoy.toLocaleDateString())
 
-const fecha_ahora = new Date();
+let fecha_nacim = new Date(1986, 06, 29)
+console.log(fecha_nacim.toLocaleDateString())
 
-console.log(fecha_ahora);
-
-const cumpleanios = new Date(1986, 6, 29, 12, 51, 27, 37);
-
-console.log(cumpleanios);
-
-// new date con milisegundos
-const fecha3 = new Date(0+300*60*60*60+100000000000*10);
-console.log(fecha3);
-
-// new date con strings
-const fecha4 = new Date("October 13, 1979 12:15:40");
-console.log(fecha4);
-
-// Comparar
-if (fecha4 < fecha_ahora)
-{
-    console.log(true);
+if (fecha_hoy > fecha_nacim) {
+    console .log(true);
 }
 
-console.log(fecha_ahora.getTime())
-
-// Mostrar fechas
-
-console.log(fecha_ahora.getDate() + "/" + (fecha_ahora.getMonth() + 1))
-
-console.log(fecha_ahora.toLocaleDateString())
-console.log(fecha_ahora.toLocaleTimeString())
-console.log(fecha_ahora.toLocaleString())
+const dia_nacim = fecha_nacim.getDate();
+console.log(dia_nacim);
+const mes_nacim = fecha_nacim.getMonth() + 1;
+console.log(mes_nacim);
+const anio_nacim = fecha_nacim.getFullYear();
+console.log(anio_nacim);
